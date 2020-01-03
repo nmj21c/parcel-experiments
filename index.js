d59cf4b0648a50c2482c3f46351525b32b787001
Jonghyun <nmj21c@gmail.com>
2020-01-03 14:45:55 +0900

    add react, babel

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello world2');
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./src/App";
+
+ReactDom.render(<App />, document.getElementById('app'));
